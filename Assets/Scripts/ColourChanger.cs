ea6d8a7026ba07f576508f870de9755eff5b5a0a
Mishke <134335944+Iceglow02@users.noreply.github.com>
2025-10-23 12:25:21 +1300

    Major Changes to PowerObject and ColourChanger Scripts
    Absorbing can now be interrupted by:
    - The player exiting the absorb radius
    - The player releasing the absorb button before the action has been completed.
    
    When this happens the object will regain it's colour starting from the colour it had become while being drained (and not instantly regain the colour or snap to the FadedColour before starting to regain colour).

--- Assets/Scripts/ColourChanger.cs
+++ Assets/Scripts/ColourChanger.cs
@@ -4,50 +4,71 @@ using UnityEngine;
 public class ColourChanger : MonoBehaviour
 {
     public string powerColour;
-    public Color startingColour;
-    public Color fadedColour;
+    public Color originalColour, fadedColour, changeStartingColor;
     private Color currentColour;
     public bool faded, absorbable;
-    private float fadeTime; //How long before the colour changes fully
     Renderer objectRenderer;
     
     void Start()
     {
         absorbable = true;
         fadedColour = Color.gray;
-        fadeTime = 2.0f;
-        currentColour = startingColour;
+        currentColour = originalColour;
+        changeStartingColor = originalColour;
         objectRenderer = GetComponent<Renderer>();
         objectRenderer.material.color = currentColour;
     }
 
     void Update()
     {
+
+    }
+
+    public void SetChangeStartingColor()
+    {
+        changeStartingColor = currentColour;
+    }
+    
+    public void FadeToColour(Color colourChange, float currentTime, float changeTime)
+    {
+        absorbable = false;
+        
+        objectRenderer.material.color = Color.Lerp(changeStartingColor, colourChange, currentTime / changeTime);
+        
+        if (currentTime + Time.deltaTime >= changeTime)
+        {
+            objectRenderer.material.color = colourChange; // Ensure the final color is set
+            SetChangeStartingColor();
+        }
+        
+        currentColour = objectRenderer.material.color;
+        faded = (currentColour != originalColour);
+        absorbable = (currentColour == originalColour);
         
     }
     
-    public IEnumerator FadeToColour(Color colourChange)
-    {
-        absorbable = false;
-        float currentTime = 0f;
-        while (currentTime < fadeTime)
-        {
-            objectRenderer.material.color = Color.Lerp(currentColour, colourChange, currentTime/fadeTime);
-            currentTime += Time.deltaTime;
-            yield return null; // Wait for the next frame
-        }
-        objectRenderer.material.color = colourChange; // Ensure the final color is set
-        currentColour = colourChange;
+    // public IEnumerator FadeToColour(Color colourChange)
+    // {
+    //     absorbable = false;
+    //     float currentTime = 0f;
+    //     while (currentTime < fadeTime)
+    //     {
+    //         objectRenderer.material.color = Color.Lerp(currentColour, colourChange, currentTime/fadeTime);
+    //         currentTime += Time.deltaTime;
+    //         yield return null; // Wait for the next frame
+    //     }
+    //     objectRenderer.material.color = colourChange; // Ensure the final color is set
+    //     currentColour = colourChange;
 
-        faded = (currentColour != startingColour);
-        if (faded)
-        {
-            StartCoroutine(FadeToColour(startingColour));
-        }
+    //     faded = (currentColour != startingColour);
+    //     if (faded)
+    //     {
+    //         StartCoroutine(FadeToColour(startingColour));
+    //     }
 
-        if (currentColour == startingColour)
-        {
-            absorbable = true;
-        }
-    }
+    //     if (currentColour == startingColour)
+    //     {
+    //         absorbable = true;
+    //     }
+    // }
 }
--- Assets/Scripts/PowerObject.cs
+++ Assets/Scripts/PowerObject.cs
@@ -1,16 +1,20 @@
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using System.Collections;
 
 public class PowerObject : MonoBehaviour
 {
     private InputAction mAbsorbAction;
     ColourChanger colourChanger;
     PlayerCharacter player;
-    private bool insideAbsorbZone, absorbing;
+    private bool insideAbsorbZone, absorbing, resetColour;
+    float currentTime, fadeTime; 
 
     void Start()
     {
+        resetFadeTime();
+        currentTime = 0f;
         absorbing = false;
         mAbsorbAction = InputSystem.actions.FindAction("Absorb");
         colourChanger = GetComponentInChildren<ColourChanger>();
@@ -19,34 +23,71 @@ public class PowerObject : MonoBehaviour
 
     void Update()
     {
-        if (insideAbsorbZone)
+        AbsorbingProcess();
+        if (resetColour)
         {
-            AbsorbingProcess();
+            StartCoroutine(restoreColour(fadeTime));
         }
     }
 
-    void AbsorbingProcess() 
+    void AbsorbingProcess()
     {
+        if (!insideAbsorbZone) { return; }
+
         if (mAbsorbAction.WasPressedThisFrame() && colourChanger.absorbable)
         {
             absorbing = true;
-            Debug.Log("Pressed");
-            StartCoroutine(colourChanger.FadeToColour(colourChanger.fadedColour));
+            currentTime = 0f;
+            resetFadeTime();
         }
 
         if (!absorbing) { return; }
 
+        if (mAbsorbAction.IsPressed() && currentTime < fadeTime)
+        {
+            colourChanger.FadeToColour(colourChanger.fadedColour, currentTime, fadeTime);
+            currentTime += Time.deltaTime;
+            return;
+        }
+
         if (mAbsorbAction.WasReleasedThisFrame()) //Stopped absorbing
         {
+            interruptedAbsorbing();
+            resetColour = true;
             absorbing = false;
             return;
         }
 
-        if (mAbsorbAction.WasPerformedThisFrame()) //Player has drained colour and now can absorb power
+        if (currentTime >= fadeTime) //Player has drained colour and now can absorb power
         {
+            currentTime = 0f;
+            resetColour = true;
             player.AbsorbPower(colourChanger.powerColour);
             absorbing = false;
         }
+    }
+
+    public IEnumerator restoreColour(float fadeTime)
+    {
+        resetColour = false;
+        colourChanger.SetChangeStartingColor();
+        while (currentTime < fadeTime)
+        {
+            colourChanger.FadeToColour(colourChanger.originalColour, currentTime, fadeTime);
+            currentTime += Time.deltaTime;
+            yield return null;
+        }
+    }
+
+    void resetFadeTime()
+    {
+        fadeTime = 2.0f;
+    }
+    
+    void interruptedAbsorbing()
+    {
+        fadeTime = currentTime;
+        currentTime = 0f;
     }
 
     void OnTriggerEnter(Collider collider)
@@ -62,6 +103,8 @@ public class PowerObject : MonoBehaviour
         if (collider.CompareTag("Player"))
         {
             insideAbsorbZone = false;
+            interruptedAbsorbing();
+            resetColour = absorbing;
             absorbing = false;
         }
     }
