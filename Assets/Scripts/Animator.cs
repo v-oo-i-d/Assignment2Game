d3ef389be92bf5e9028b03ea8bfb1e1a86065f85
Michael Graham <MikeGraham.295@gmail.com>
2025-10-23 19:15:50 +1300

    Fixed player animations

--- Assets/Scripts/Animator.cs
+++ Assets/Scripts/Animator.cs
@@ -12,7 +12,20 @@ public class PlayerAnimator : MonoBehaviour
     void Update()
     {
         Vector3 playerVelocity = GetComponent<PlayerCharacter>().GetVelocity();
+        
+        // Horizontal movement only
         float velMag = new Vector3(playerVelocity.x, 0, playerVelocity.z).magnitude;
-        mAnimator.SetFloat("PlayerVelocity", velMag);
+
+        float directionalVelocity = 0f;
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            directionalVelocity = velMag;
+        }
+        else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            directionalVelocity = -velMag;
+        }
+
+        mAnimator.SetFloat("Speed", directionalVelocity);
     }
 }
