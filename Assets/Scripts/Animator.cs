597df28c960ff33877c44eb2ef76392fea15102a
Michael Graham <MikeGraham.295@gmail.com>
2025-10-24 19:33:19 +1300

    Finished basic animations + added floor texture

--- Assets/Scripts/Animator.cs
+++ Assets/Scripts/Animator.cs
@@ -12,21 +12,21 @@ public class PlayerAnimator : MonoBehaviour
         // Set animation speed multipliers based on player movement speed
         mAnimator.SetFloat("MoonwalkMultiplier", playerMoveSpeed * 0.243f);
         mAnimator.SetFloat("WalkMultiplier", playerMoveSpeed * 0.243f);
-        mAnimator.SetFloat("StrafeMultiplier", playerMoveSpeed * 0.5f);
+        mAnimator.SetFloat("LeftStrafeMultiplier", playerMoveSpeed * 0.5f);
+        mAnimator.SetFloat("RightStrafeMultiplier", playerMoveSpeed * 0.2f);
     }
 
     void Update()
     {
         // Vector3 playerVelocity = GetComponent<PlayerCharacter>().GetVelocity();
         
-        float inputX = Input.GetAxisRaw("Horizontal");  // A/D or Left/Right
-        float inputZ = Input.GetAxisRaw("Vertical");    // W/S or Up/Down
+        float inputX = Input.GetAxisRaw("Horizontal");
+        float inputZ = Input.GetAxisRaw("Vertical");
 
         Vector3 playerVelocity = GetComponent<PlayerCharacter>().GetVelocity();
         float speed = new Vector3(playerVelocity.x, 0, playerVelocity.z).magnitude;
 
         mAnimator.SetFloat("ForwardVelocity", inputZ * speed);
         mAnimator.SetFloat("SidewaysVelocity", inputX * speed);
-        Debug.Log(mAnimator.GetFloat("SidewaysVelocity"));
     }
 }
