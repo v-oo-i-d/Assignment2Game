0dc71c48b07fa43497b586bc9f79092e4a6bd85c
Kale Twist <juniortwistit@gmail.com>
2025-11-08 00:11:58 +1300

    Added Keypad

---
+++ Assets/Scripts/MainDoorButton.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+public enum ButtonType {Orange, Green, Purple, Reset}
+public class MainDoorButton : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public ButtonType buttonType;
+    private float maxDistance = 7.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            if (LookingAtButton()) UseButton();
+        }
+    }
+
+    private bool LookingAtButton()
+    {
+        Camera cam = Camera.main;
+        Ray ray = new(cam.transform.position, cam.transform.forward);
+
+        if (Physics.Raycast(ray, out RaycastHit hit, maxDistance))
+        {
+            if (hit.transform == this.transform)
+            {
+                Debug.Log(buttonType);
+                return true;
+            }
+        }
+
+        return false;
+    }
+    private void UseButton()
+    {
+        if (buttonType == ButtonType.Reset)
+        {
+            MainDoorKeypad.code.Clear();
+        }else
+        {
+            MainDoorKeypad.code.Add(buttonType);
+        }
+    }
+}
--- Assets/Scripts/MainDoorKeypad.cs
+++ Assets/Scripts/MainDoorKeypad.cs
@@ -1,16 +1,72 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class MainDoorKeypad : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public static List<ButtonType> code = new List<ButtonType>(4);
+    public static List<ButtonType> correctCode = new List<ButtonType> { ButtonType.Green, ButtonType.Purple, ButtonType.Orange, ButtonType.Orange };
+    public static List<GameObject> display = new List<GameObject>(4);
+    public GameObject door;
+    public Material orange, purple, green, gray;
+
     void Start()
     {
-        
+        display.Add(transform.Find("First")?.gameObject);
+        display.Add(transform.Find("Second")?.gameObject);
+        display.Add(transform.Find("Third")?.gameObject);
+        display.Add(transform.Find("Fourth")?.gameObject);
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        int displayNumber = 0;
+        foreach (ButtonType button in code)
+        {
+            Material input = display[displayNumber].GetComponent<Renderer>().material;
+            switch (button)
+            {
+                case ButtonType.Green:
+                    input.color = green.color;
+                    break;
+                case ButtonType.Purple:
+                    input.color = purple.color;
+                    break;
+                case ButtonType.Orange:
+                    input.color = orange.color;
+                    break;
+                default:
+                    input.color = gray.color;
+                    break;
+            }
+            displayNumber++;
+        }
+        while (displayNumber < 4)
+        {
+            display[displayNumber].GetComponent<Renderer>().material.color = gray.color;
+            displayNumber++;
+        }
+        if (code.Count == 4)
+        {
+            int amountCorrect = 0;
+            for (int i = 0; i < 4; i++)
+            {
+                if (code[i] == correctCode[i])
+                {
+                    amountCorrect++;
+                }
+            }
+            if (amountCorrect == 4)
+            {
+                OpenDoor();
+            }
+            else
+            {
+                code.Clear();
+            }
+        }
+    }
+    public void OpenDoor()
+    {
+        door.GetComponent<Door>().Unlock();
+        Destroy(gameObject);
     }
 }
